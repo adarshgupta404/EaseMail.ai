6dd1c643f7988eb08ca5baf8cf581fabc5df6208
adarshgupta404 <gadarsh747@gmail.com>
2025-02-27 18:40:05 +0530

    .

--- src/app/api/aurinko/callback/route.ts
+++ src/app/api/aurinko/callback/route.ts
@@ -5,8 +5,9 @@ import { auth } from "@clerk/nextjs/server";
 import axios from "axios";
 import { type NextRequest, NextResponse } from "next/server";
 import { waitUntil } from "@vercel/functions";
+
 export const config = {
-  maxDuration: 5,
+  maxDuration: 59,
 };
 
 export const GET = async (req: NextRequest) => {
